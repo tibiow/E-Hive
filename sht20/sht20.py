1c0fced98209b282f19929abe309975b1745efbf
piernov <piernov@piernov.org>
2019-01-28 23:44:04 +0000

    sht20 both sensors

--- sht20/sht20.py
+++ sht20/sht20.py
@@ -8,56 +8,32 @@ from sensor.SHT20 import SHT20
 import smbus
 import time
 
-# Define some constants from the datasheet
-
-DEVICE     = 0x23 # Default device I2C address
-
-POWER_DOWN = 0x00 # No active state
-POWER_ON   = 0x01 # Power on
-RESET      = 0x07 # Reset data register value
-
-# Start measurement at 4lx resolution. Time typically 16ms.
-CONTINUOUS_LOW_RES_MODE = 0x13
-# Start measurement at 1lx resolution. Time typically 120ms
-CONTINUOUS_HIGH_RES_MODE_1 = 0x10
-# Start measurement at 0.5lx resolution. Time typically 120ms
-CONTINUOUS_HIGH_RES_MODE_2 = 0x11
-# Start measurement at 1lx resolution. Time typically 120ms
-# Device is automatically set to Power Down after measurement.
-ONE_TIME_HIGH_RES_MODE_1 = 0x20
-# Start measurement at 0.5lx resolution. Time typically 120ms
-# Device is automatically set to Power Down after measurement.
-ONE_TIME_HIGH_RES_MODE_2 = 0x21
-# Start measurement at 1lx resolution. Time typically 120ms
-# Device is automatically set to Power Down after measurement.
-ONE_TIME_LOW_RES_MODE = 0x23
-
-#bus = smbus.SMBus(0) # Rev 1 Pi uses 0
-bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
-
 myurl = "http://raspberrymiaou.local:1880/sensors"
 
-def readTemperature(addr=DEVICE):
-  sht = SHT20(0, 0x40)
+def readTemperature(bus):
+  sht = SHT20(bus, 0x40)
   t = sht.temperature()
   return t
+
+def sendTemp(bus):
+  temperatureLevel=readTemperature(bus)
+  print("Temperature" + str(bus) + " : " + format(temperatureLevel) + " lx")
+
+  body = {'type': 'Temperature', 'sensorId': bus, 'value': temperatureLevel, 'date': datetime.datetime.now().isoformat()}
+
+  req = urllib.request.Request(myurl)
+  req.add_header('Content-Type', 'application/json; charset=utf-8')
+
+  jsondata = json.dumps(body)
+  jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+  req.add_header('Content-Length', len(jsondataasbytes))
+  response = urllib.request.urlopen(req, jsondataasbytes)
 
 def main():
 
   while True:
-    temperatureLevel=readTemperature()
-    print("Temperature : " + format(temperatureLevel) + " lx")
-
-    body = {'type': 'Temperature', 'sensorId': 1, 'value': temperatureLevel, 'date': datetime.datetime.now().isoformat()}
-
-    req = urllib.request.Request(myurl)
-    req.add_header('Content-Type', 'application/json; charset=utf-8')
-
-    jsondata = json.dumps(body)
-    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-    req.add_header('Content-Length', len(jsondataasbytes))
-    response = urllib.request.urlopen(req, jsondataasbytes)
-
+    sendTemp(0)
+    sendTemp(1)
     time.sleep(10)
 
 if __name__=="__main__":
