ab344408fd10e78a5fe25ce32b62b04f3c0e58e7
piernov <piernov@piernov.org>
2019-02-05 15:12:04 +0100

    start-sensors.sh exit if one script dies

--- sensors/start-sensors.sh
+++ sensors/start-sensors.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 python3 bh1750/bh1750.py &
 python3 sht20/sht20.py &
-python3 hx711/example.py
+python3 hx711/example.py &
+
+# python3 scripts shouldn't exit, if they did quit the container, it'll be restart automatically
+wait -n
+killall python3
+exit 1
